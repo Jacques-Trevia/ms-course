7918203a677cfb85976c03c21c3e59d2d946bba3
Jacques-Trevia <jacquesceara@gmail.com>
2026-04-24 22:28:51 -0300

    Feign Client

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/dto/WorkerDTO.java
@@ -0,0 +1,42 @@
+package com.devsuperior.hrpayroll.dto;
+
+import com.devsuperior.hrpayroll.entities.Worker;
+
+public class WorkerDTO {
+
+		private Long id;
+		private String name;
+		private Double dailyIncome;
+		
+		public WorkerDTO() {
+		}
+
+		public WorkerDTO(Long id, String name, Double dailyIncome) {
+			this.id = id;
+			this.name = name;
+			this.dailyIncome = dailyIncome;
+		}
+		
+		public WorkerDTO(Worker entity) {
+			id = entity.getId();
+			name = entity.getName();
+			dailyIncome = entity.getDailyIncome();
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public Double getDailyIncome() {
+			return dailyIncome;
+		}
+	}
+
